43a9c7752eefae5cc2ba4640bc2bbe91a5ec5390
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-04-21 02:23:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (47.22%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> mp;
+
+    for (int i = 0; i < order.size(); i++) {
+        mp[order[i]] = i;
+    }
+
+    for (int i = 0; i < words.size() - 1; i++) {
+        string w1 = words[i];
+        string w2 = words[i + 1];
+
+        int j = 0;
+        bool foundDifference = false;
+
+        while (j < w1.size() && j < w2.size()) {
+            if (w1[j] != w2[j]) {
+                if (mp[w1[j]] > mp[w2[j]]) {
+                    return false;
+                }
+                foundDifference = true;
+                break;
+            }
+            j++;
+        }
+
+        if (!foundDifference && w1.size() > w2.size()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+    
+};
