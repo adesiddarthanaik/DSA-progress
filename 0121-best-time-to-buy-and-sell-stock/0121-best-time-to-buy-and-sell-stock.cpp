a35f696f596a7e8dc16ab752e6fab53ca43601f5
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-08-04 22:49:33 +0530

    Time: 0 ms (100%), Space: 97.4 MB (25.92%) - LeetHub - Time: O(n), Space: O(1)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -13,3 +13,8 @@ public:
         return profit;  
     }
 };
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
