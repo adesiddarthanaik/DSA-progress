b2063f6336803c42948367994498e3b14f82710e
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-08-04 22:24:45 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.89%) - LeetHub - Time: O(n), Space: O(1)

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr_sum = 0;
+        int curr_sum =0;
         int max_sum = nums[0];
 
         for(int i=0;i<nums.size();i++){
-            if (curr_sum<0){
-                curr_sum =0 ;
+            if(curr_sum < 0){
+                curr_sum = 0;
             }
             curr_sum = curr_sum+nums[i];
             max_sum = max(curr_sum,max_sum);
         }
+
         return max_sum;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
