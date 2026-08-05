d9fdaf53a84e3fd85fac6734b8adac361dc360a0
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-08-05 20:09:08 +0530

    Time: 0 ms (100%), Space: 31.3 MB (82.34%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;   // Move left
+            }
+            else {
+                low = mid + 1;    // Move right
+            }
+        }
+
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
