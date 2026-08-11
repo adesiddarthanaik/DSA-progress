eccf729bd72e2fc88072ce24d6587397f308cee3
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-08-11 19:32:17 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.6%) - LeetHub - Time: O(n), Space: O(1)

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i =0 ;
-        for(int j =1;j<nums.size();j++){
-            if(nums[i]!= nums[j]){
+        int i = 0;
+        int j = 1;
+
+        while(j < nums.size()){
+            if(nums[i] == nums[j]){
+                j++;
+            }
+            else{
                 i++;
                 nums[i] = nums[j];
+                j++;
             }
         }
         return i+1;
