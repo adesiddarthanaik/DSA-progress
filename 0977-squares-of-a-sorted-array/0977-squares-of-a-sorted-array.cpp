6e96caee70ca782f4fcd069edb916f4c62cbcaa8
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-09-23 17:38:57 +0530

    Time: 0 ms (100%), Space: 30.4 MB (29.03%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        int start=0;
+        int end=n-1;
+        
+        vector <int> res(n);
+        int pos = n-1;
+        
+        while(start <= end)
+        {
+            if(abs(nums[start]) < abs(nums[end])) {
+                res[pos--] = nums[end] * nums[end];
+                end--;
+            } else{
+                res[pos--] = nums[start] * nums[start];
+                start++;
+            }
+        }
+        return res;
+    }
+    };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
