6a333c9dc38ac5acde897a76e68d35af9f4809d3
ADE SIDDARTHA NAIK <siddathaade@gmail.com>
2026-09-24 15:53:36 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.13%) - LeetHub - Time: O(n + m), Space: O(n + m)

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:    
+    string getString(string &str) {
+        int n=str.length(), count=0;
+        string result="";
+        for(int i=n-1; i>=0; i--) {
+            char ch=str[i];
+            if(ch=='#') 
+                count++;
+            else {
+                if(count>0)
+                    count--;
+                else {
+                    result+=ch;
+                }                     
+            }
+        }
+        return result;        
+    }
+    
+    bool backspaceCompare(string S, string T) {
+        return getString(S)==getString(T);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
